5000ca6c783268f459f1635919464f44d2f7abc7
ehu <ehu@158.227.125.145>
2025-10-03 12:52:36 +0200

    agrego la clase test para el metodo egoeraEzarri

---
+++ src/test/java/test/egoeraEzarriTest.java
@@ -0,0 +1,14 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class egoeraEzarriTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
